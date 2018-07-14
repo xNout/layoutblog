2877dbf01647d94cc4ced773bd1f118f7c20032d
xNout <vera254d4@gmail.com>
2018-07-13 21:52:26 -0500

    Archivos necesarios para el layout basico

---
+++ js/scripts.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+
+	// var fondos = [
+	// 	"../img/fondo_1.jpg",
+	// 	"../img/fondo_2.jpg",
+	// 	"../img/fondo_3.png"
+	// ];
+
+	// var background_inicio = $(".banner .img");
+	// var img = fondos[ Math.floor((Math.random() * 2) + 0) ];
+
+	// background_inicio.css({
+	// 	backgroundImage: img
+	// });
+
+	// console.log( img );
+
+	// check_window_size( );
+
+	// if( $(document).scrollTop() >= ( $(".body").offset().top - $(".nav").height() ) ){
+	// 	check_( "on_menu_fixed");
+	// 	$(".btn-up").fadeIn(300);
+	// }
+	// else{
+	// 	$(".btn-up").fadeOut(300);
+	// }
+
+	// $(document).on("scroll", function(){
+	// 	if( $(document).scrollTop() >= ( $(".body").offset().top - $(".nav").height() ) ){
+	// 		check_( "on_menu_fixed");
+	// 	}
+	// 	else{
+	// 		check_( "off_menu_fixed");
+	// 	}
+	// });
+
+	// $(window).on("resize", function(){
+	// 	check_window_size( );
+	// });
+
+	// $(".btn-up").on("click", function(){
+	// 	$(".btn-up").hide();
+	// 	$("html, body").animate({ scrollTop: 0 }, 200);
+	// });
+
+	// function check_window_size( ){
+	// 	var size = $(window).width(); 
+	// 	var smallNav = $(".nav-xs");
+	// 	if( size <= 795 ){
+	// 		smallNav.removeClass("hide");
+	// 	}
+	// 	else{
+	// 		smallNav.addClass("hide");
+	// 	}
+	// }
+
+
+	// $(".btn_show_menu").on("click", function(e){
+	// 	e.preventDefault();
+	// 	$(".nav_xs_items").slideToggle(500);
+	// });
+
+
+
+	// function check_( event ){
+	// 	switch ( event ) {
+	// 		case "on_menu_fixed":
+	// 			$(".nav").addClass("menu_fixed");
+	// 			$(".body").addClass("on_menu_fixeable");
+	// 			$(".btn-up").fadeIn(300);
+	// 			break;
+
+	// 		case "off_menu_fixed":
+	// 			$(".nav").removeClass("menu_fixed");
+	// 			$(".body").removeClass("on_menu_fixeable");
+	// 			$(".btn-up").fadeOut(300);
+	// 			break;
+	// 	}
+	// }
+});
